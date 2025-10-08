caf22178c1f289642196f6bc234da77efc513900
kix300 <kixwalkiki@gmail.com>
2025-10-08 14:12:38 +0200

    add docker

--- install.sh
+++ install.sh
@@ -14,6 +14,20 @@ apt update && apt upgrade -y
 echo "Installation des outils de développement..."
 apt install -y git make gcc g++ libreadline-dev clang valgrind vscode
 
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 # Installation de Fish shell
 echo "Installation de Fish shell..."
 apt install -y fish
